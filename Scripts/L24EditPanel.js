2e9909eafd91795d91d9e839b3c05f5b43a213ba
James Ellis-Jones <jamesej@regulussystems.com>
2012-02-08 23:55:22 +0000

    Improve link editor

---
+++ Scripts/L24EditPanel.js
@@ -0,0 +1,18 @@
+﻿(function($) {
+    $(document).ready(function() {
+        $('#newButton').click(function() {
+            var createUrl = prompt("Enter url of new page");
+        });
+		
+		var showLinkFields = function () {
+			$link = $(this).closest('.l24-link');
+			var isint = $link.find('.l24-link-isinternal input').attr('checked');
+			$link.find('.l24-link-controller')[isint ? 'show' : 'hide']();
+			$link.find('.l24-link-action')[isint ? 'show' : 'hide']();
+			$link.find('.l24-link-url')[isint ? 'hide' : 'show']();
+		}
+		
+		$('.l24-link').each(showLinkFields);
+		$('.l24-link-isinternal input').click(showLinkFields);
+    });
+})(jQuery);
