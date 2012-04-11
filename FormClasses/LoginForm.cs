0b176495e76b1c6fa25b180ba210e83e8f6a3396
James Ellis-Jones <jamesej@regulussystems.com>
2012-04-11 11:34:00 +0100

    Login form, don't use contentdisplaynameattribute

--- FormClasses/LoginForm.cs
+++ FormClasses/LoginForm.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using L24CM.Attributes;
+using System.ComponentModel;
 
 namespace L24CM.FormClasses
 {
     public class LoginForm
     {
-        [ContentDisplayName("UserName", typeof(LoginForm))]
+        //[ContentDisplayName("UserName", typeof(LoginForm))]
+        [DisplayName("User Name")]
         public string UserName { get; set; }
-        [ContentDisplayName("Password", typeof(LoginForm))]
+        //[ContentDisplayName("Password", typeof(LoginForm))]
+        [DisplayName("Password")]
         public string Password { get; set; }
     }
 }
